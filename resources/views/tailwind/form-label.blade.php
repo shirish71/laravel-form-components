eba4955054e52e93c31148122439b870ecbaa0ae
Shirish Maharjan <shirishmaharjan71@gmail.com>
2021-07-07 15:31:31 +0545

    first commit

--- resources/views/tailwind/form-label.blade.php
+++ resources/views/tailwind/form-label.blade.php
@@ -1,4 +1,7 @@
-<label {!! $attributes->merge(['class' => 'label block mb-2 ']) !!}>
+<label {!! $attributes->merge([
+    'class' => 'label block mb-2 ',
+    'for'=>$for
+    ]) !!}>
     {{ $label }}@if($required)<i class="text-red-500">*</i>@endif
 </label>
 
